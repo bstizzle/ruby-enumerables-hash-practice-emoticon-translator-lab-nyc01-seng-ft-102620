05ce7ea17af053153d1ae607a164629bd35e0cd8
Benjamin Stein <whereyouben@gmail.com>
2020-10-21 02:14:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,7 @@ require 'yaml'
 def load_library(path)
   # code goes here
   emos = YAML.load_file(path)
-  emos.map do |k, v|
-    emos[k] = {:english => v[0], :japanese => v[1]}
-  end
-  emos
+  emos.map {|k, v| emos[k] = {:english => v[0], :japanese => v[1]} }
 end
 
 def get_japanese_emoticon
